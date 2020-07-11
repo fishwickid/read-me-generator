bb82c8bc77f7695f1525d45cc3cb33c918a4b318
fishwickid <chadfishwick@gmail.com>
2020-07-11 16:44:26 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-
-# Project Title: Generator of readme\n ### Project Description:\n it's cool \n ### Installation Details:\n no requirements\n ### Usage Information:\n it's easy \n ### Licences Required:\n \n  ### Github:\n fishwickid\n ### Contact Info:\n chadfishwick@gmail.com
+   c\n \n    ### Licences Required:\n \n    \n  \n    ### Github:\n \n    e\n \n    ### Contact Info:\n \n    f
+\n    # Project Title: \n    hello\n    ## Project Description: \n    a\n    ### Installation Details:\n    b\n    ### Usage Information:\n    c\n    ### Licences Required:\n      \n    ### Github: \n    e\n    ### Contact Info:\n    f
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-    return `# Project Title: ${data.title}\n ### Project Description:\n ${data.description}\n ### Installation Details:\n ${data.installation}\n ### Usage Information:\n ${data.usage}\n ### Licences Required:\n ${data.licences}\n  ### Github:\n ${data.github}\n ### Contact Info:\n ${data.contact}`;
+    return `
+    # Project Title: 
+    ${data.title}
+    ## Project Description: 
+    ${data.description}
+    ### Installation Details:
+    ${data.installation}
+    ### Usage Information:
+    ${data.usage}
+    ### Licences Required:
+    ${data.licences}  
+    ### Github: 
+    ${data.github}
+    ### Contact Info:
+    ${data.contact}`;
 }
 
 module.exports = generateMarkdown;
